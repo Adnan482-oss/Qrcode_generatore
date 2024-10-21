0b60df1100c8549018de3172fd877c0d380d02cb
Rafshan Jani <141335206+Adnan482-oss@users.noreply.github.com>
2024-10-21 11:09:06 +0600

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,11 +1,20 @@
 import qrcode
 from PIL import Image
 
-qr=qrcode.QrCode(version=1,error_correction=qrcode.constant.CORRECT_ERROR_H,
-box_size=10,border=1)
+# Create a QRCode object with specified parameters
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # Corrected the constant name
+    box_size=10,
+    border=1
+)
 
-qr.add_data("")
-qr.make(True)
+# Add data to the QR code
+qr.add_data("https://www.google.com")  # It's good practice to use "https://" for URLs
 
-image=qr.make_image(fill_color="black",back_color="white")
-image.save("")
+# Make the QR code
+qr.make(fit=True)
+
+# Customize the QR code's appearance and save it as an image
+image = qr.make_image(fill_color="black", back_color="white")
+image.save("name.png")
